31399878b1bc0a50c66336084d25dc60427d6508
mikaelbeat <ryynanenphm@gmail.com>
2023-01-03 22:32:00 +0200

    Update

--- Days_21-30/Tkinter/main.py
+++ Days_21-30/Tkinter/main.py
@@ -1,12 +1,29 @@
 
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("*** My first GUI program ***")
 window.minsize(width=500, height=400)
 
-my_label = tkinter.Label(text="Test label", font=("Arial", 25, "bold"))
+my_label = Label(text="Test label", font=("Arial", 25, "bold"))
 my_label.pack(side="left")
+my_label["text"] = "Kiisseli"
+
+
+def button_clicked():
+    data = input.get()
+    my_label["text"] = data
+    
+
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+
+input = Entry(width=50)
+input.pack()
+
 
 
 
