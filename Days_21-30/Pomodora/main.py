5b45696766df3e05f11ca09698083fafb8d59e67
mikaelbeat <ryynanenphm@gmail.com>
2023-10-13 14:24:10 +0300

    Update

--- Days_21-30/Pomodora/main.py
+++ Days_21-30/Pomodora/main.py
@@ -1,6 +1,6 @@
 
 from tkinter import *
-
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 TOMATO_IMAGE = "Days_21-30/Pomodora/tomato.png"
@@ -19,8 +19,21 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def count_down(count):
+    
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+    
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -33,13 +46,16 @@ tomato_img = PhotoImage(file=TOMATO_IMAGE)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.grid(column=1, row=1)
 
+
+
 title_label = Label(text="TIMER", bg=YELLOW, fg=GREEN, font=("Arial", 25, "bold"))
 title_label.grid(column=1, row=0)
 
 
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
-start_button = Button(text="Start", font=("Arial", 20, "bold"))
+start_button = Button(text="Start", font=(
+    "Arial", 20, "bold"), command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", font=("Arial", 20, "bold"))
