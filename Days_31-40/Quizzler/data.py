66c1e5b5423d3f426b31cd403f5e4273a927befb
mikaelbeat <ryynanenphm@gmail.com>
2023-12-04 16:17:33 +0200

    Update

--- Days_31-40/Quizzler/data.py
+++ Days_31-40/Quizzler/data.py
@@ -1,32 +1,13 @@
 import requests
 
-api_response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
-json_data = api_response.json()
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+json_data = response.json()
 
 
 question_data = json_data["results"]
-
-print(json_data["results"])
-
-# question_data = [
-#     {
-#         "category": "Science: Computers",
-#         "type": "boolean",
-#         "difficulty": "medium",
-#         "question": "The HTML5 standard was published in 2014.",
-#         "correct_answer": "True",
-#         "incorrect_answers": [
-#             "False"
-#         ]
-#     },
-#     {
-#         "category": "Science: Computers",
-#         "type": "boolean",
-#         "difficulty": "medium",
-#         "question": "The first computer bug was formed by faulty wires.",
-#         "correct_answer": "False",
-#         "incorrect_answers": [
-#             "True"
-#         ]
-#     }
-# ]
