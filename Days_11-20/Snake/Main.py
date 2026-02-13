9ae26f0628742c25d2b355bb845e0c6e598773c9
mikaelbeat <ryynanenphm@gmail.com>
2026-02-13 22:22:14 +0200

    Update

--- Days_11-20/Snake/Main.py
+++ Days_11-20/Snake/Main.py
@@ -9,9 +9,12 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-
-
 snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
